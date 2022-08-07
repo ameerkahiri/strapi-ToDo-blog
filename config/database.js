8a37488b3fd1ba011f2b2b0016abd798da8d07bc
ameerkahiri <ameer_alien@yahoo.com>
2022-08-07 17:35:17 +0800

    update to asal

--- config/database.js
+++ config/database.js
@@ -16,10 +16,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      socketPath: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
+      host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USER'),
       password: env('DATABASE_PASSWORD'),
+      //ssl: false,
     },
   },
 });
